42b3a26b43a2dd325232f24745021055752707e7
atpapiern <atpapiern@coastal.edu>
2025-09-03 23:44:01 -0400

    Adding Business Card App

--- Week2/BusinessCard/App.js
+++ Week2/BusinessCard/App.js
@@ -1,20 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, Linking} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <SafeAreaView style={styles.root}>
+        <View style={styles.imageContainer}>
+          <Image 
+            style={styles.image}
+            source={require("./assets/images/headshot.jpg")}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.name}>Alex Papiernik</Text>
+          <Text 
+            style={styles.text}
+            onPress={() => {
+              Linking.openURL("mailto:atpapiern@coastal.edu");
+            }}
+            >atpapiern@coastal.edu</Text>
+          <Text 
+            style={styles.text}
+            onPress={() => {
+              Linking.openURL("tel:7048160467");
+            }}
+            >(704) 816-0467</Text>
+          <Text 
+            style={styles.text}
+            onPress={() => {
+              Linking.openURL("https://github.com/atpapiern");
+            }}
+            >My GitHub Link</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    flex: 1, 
+    justifyContent: "center", 
+    alignItems: "center", 
+    backgroundColor: "#006f71"
   },
+  imageContainer: {
+    flex: 2,
+    marginTop: 50, 
+    padding: 50,
+    width: "100%"
+  },
+  image: {
+    height: 310, 
+    width: "100%",
+    resizeMode: "center",
+    borderColor: "black",
+    borderWidth: 5
+  },
+  textContainer: {
+    flex: 2,
+    alignItems: "center"
+  },
+  name: {
+    fontSize: 50,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 50
+  }, 
+  text: {
+    textAlign: "center", 
+    fontSize: 30, 
+    fontStyle: "italic", 
+    marginBottom: 20
+  }
 });
